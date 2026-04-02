e2a691f03f5bbf8e5a918f4b4ca796fd4fbcf923
1996vikas0 <vp2841144@gmail.com>
2026-04-02 17:58:19 +0530

    Added the search functionality in the sub promoter list

--- src/app/apphomelayout/subpromoters/subpromoters.component.ts
+++ src/app/apphomelayout/subpromoters/subpromoters.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { PromoterModel } from 'src/app/models/promoter.model';
 import {
   selectAllPromoter,
@@ -8,6 +8,7 @@ import {
   selectPromoterLoading,
 } from './state/promoter.selectors';
 import { loadPromoterStartAction } from './state/promoter.actions';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-subpromoters',
@@ -21,11 +22,35 @@ export class SubpromotersComponent implements OnInit {
   loading$!: Observable<boolean>;
   error$: Observable<string | null>;
 
+  private searchTerm$ = new BehaviorSubject<string>('');
+  filteredPromoters$!: Observable<PromoterModel[] | []>;
+
   ngOnInit(): void {
     this.store.dispatch(loadPromoterStartAction());
 
     this.allPromoters$ = this.store.select(selectAllPromoter);
     this.loading$ = this.store.select(selectPromoterLoading);
     this.error$ = this.store.select(selectPromoterError);
+
+    // combine search term with promoters list
+    this.filteredPromoters$ = combineLatest([
+      this.allPromoters$,
+      this.searchTerm$,
+    ]).pipe(
+      map(([promoters, term]) =>
+        promoters.filter((p) => {
+          const name = p?.name?.toLowerCase() || '';
+          const mobile = p?.mobile?.toLowerCase() || '';
+          const search = term.toLowerCase();
+
+          return name.includes(search) || mobile.includes(search);
+        }),
+      ),
+    );
+  }
+
+  onSearch(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.searchTerm$.next(input.value);
   }
 }
