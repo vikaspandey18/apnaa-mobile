ecd4d1f5750a3e8afec19735c883090f5053a1d5
vikaspande18 <vp2841144@gmail.com>
2026-06-12 17:45:37 +0530

    Adding the category section for the event

--- src/app/appinnerlayout/blog/services/events.service.ts
+++ src/app/appinnerlayout/blog/services/events.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiResposne } from 'src/app/models/api-response.model';
+import { CategoryModel } from 'src/app/models/category.model';
 import { EventResponse } from 'src/app/models/event.model';
 import { environment } from 'src/environments/environment.prod';
 
@@ -16,5 +17,10 @@ export class EventsService {
   getEvents(): Observable<ApiResposne<EventResponse[]>> {
     const url = `${this.baseUrl}/event/allEvent.php`;
     return this.http.get<ApiResposne<EventResponse[]>>(url);
+  }
+
+  getAllCategory(): Observable<ApiResposne<CategoryModel[]>> {
+    const url = `${this.baseUrl}/category/getAllCategory.php`;
+    return this.http.get<ApiResposne<CategoryModel[]>>(url);
   }
 }
--- src/app/appinnerlayout/blog/state/event.state.ts
+++ src/app/appinnerlayout/blog/state/event.state.ts
@@ -1,13 +1,16 @@
+import { CategoryModel } from 'src/app/models/category.model';
 import { EventResponse } from 'src/app/models/event.model';
 
 export interface EventState {
   events: EventResponse[] | [];
+  category: CategoryModel[] | [];
   loading: boolean;
   error: string | null;
 }
 
 export const initialState: EventState = {
   events: [],
+  category: [],
   loading: false,
   error: null,
 };
