0e0afd7e3904b1dec8e02d3d813590a7d4ce864f
vikaspande18 <vp2841144@gmail.com>
2026-07-07 11:27:38 +0530

    Adding the report files

--- src/app/apphomelayout/report/report.component.ts
+++ src/app/apphomelayout/report/report.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit {
+  reportUrl: SafeResourceUrl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sanitizer: DomSanitizer) {
+    this.reportUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://apnaaentry.vizitlog.com/report-ui.php',
+    );
   }
 
+  ngOnInit(): void {
+    window.location.href =
+    'https://apnaaentry.vizitlog.com/report-ui.php';
+  }
 }
